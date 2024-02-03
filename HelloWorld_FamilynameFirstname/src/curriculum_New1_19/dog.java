56be7f4c1f535c1a667fe5ee31cf185873441939
nmsoa <nmsoa@GeimiBookPro02>
2024-02-03 23:32:36 +0900

    JAVA

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_New1_19/dog.java
@@ -0,0 +1,14 @@
+package curriculum_New1_19;
+
+public class dog {
+
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	public String name;
+
+	// Q2：フィールドに動物の数の変数を定義してください。
+	public int count;
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public dog() {
+		this.name = "犬";  }
+}
