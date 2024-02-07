db27737ef0b57574ddab47ac8d77e6ca7bce9f65
nmsoa <nmsoa@GeimiBookPro02>
2024-02-07 22:22:15 +0900

    JAVA 1_23 課題2

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_New1_23/Capsule2.java
@@ -0,0 +1,19 @@
+package curriculum_New1_23;
+
+public class Capsule2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Capsule capsule = new Capsule();
+		capsule.setName("ライオン");
+		capsule.setBodylength(2.1);
+		capsule.setSpeed(80);
+		
+		System.out.println("動物名:" + capsule.getName());
+		System.out.println("体長:" + capsule.getBodylength() + "m");
+		System.out.println("速度:" + capsule.getSpeed() + "km/h");
+	}
+
+}
+
