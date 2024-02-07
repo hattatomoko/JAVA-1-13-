757ec558dd93552bafe3e070c1e976cc8ae30f64
nmsoa <nmsoa@GeimiBookPro02>
2024-02-08 01:25:36 +0900

    JAVA 1_25 Super

---
+++ HelloWorld_FamilynameFirstname/src/suppersub/Super.java
@@ -0,0 +1,41 @@
+package suppersub;
+
+public class Super {
+	private int HP;
+	private int MP;
+	private int power;
+	private int speed;
+	private int defense;
+
+	public int getHp() {
+		return this.HP;
+	}
+	public void setHp(int HP) {
+		this.HP = HP;
+	}
+	public int getMp() {
+		return this.MP;
+	}
+	public void setMp(int MP) {
+		this.MP = MP;
+	}
+	public int getPower() {
+		return this.power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getSpeed() {
+		return this.speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getDefense() {
+		return this.defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+}
