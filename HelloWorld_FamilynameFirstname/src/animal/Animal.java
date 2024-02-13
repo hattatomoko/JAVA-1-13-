085eeba0af0410f64240dafd7f65387c91f88369
nmsoa <nmsoa@GeimiBookPro02>
2024-02-13 23:16:28 +0900

    JAVA 課題1_27 animal

---
+++ HelloWorld_FamilynameFirstname/src/animal/Animal.java
@@ -0,0 +1,39 @@
+package animal;
+
+public class Animal {
+
+	protected String name;
+	protected double bodylength;
+	protected int speed;
+	protected String scientific; 
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBodylength() {
+		return this.bodylength;
+	}
+
+	public void setBodylength(double bodylength) {
+		this.bodylength = bodylength;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getScientfic() {
+		return this.scientific;
+	}
+	public void setScientfic(String scientific) {
+		this.scientific = scientific;
+	}
+
+}
