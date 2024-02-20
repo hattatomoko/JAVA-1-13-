51c3bd57dfd53bf70061d65f5bff42bee125c828
nmsoa <nmsoa@GeimiBookPro02>
2024-02-20 23:22:55 +0900

    JAVA Person

---
+++ HelloWorld_FamilynameFirstname/src/selfIntroduction/Person.java
@@ -0,0 +1,39 @@
+package selfIntroduction;
+
+public class Person {
+		// インスタンスフィールドを定義
+		public String name;
+		public int age;
+		public double height;
+		public double weight;
+		public double bmi;
+		public int print;
+	    public static int count;
+
+		
+
+		// コンストラクタを定義しインスタンスフィールドに値をセット
+		Person(String name, int age, double height,double weight){
+			this.name = name;
+			this.age = age;
+			this.height = height;
+			this.weight = weight;
+			Person.count ++;
+		}
+		
+		//bmiの定義
+		public double bmi() {
+			return this.weight / (this.height * this.height);
+			
+		}
+		
+		//インスタンスメソッドprintから（戻り値void)
+		public void print() {
+			System.out.println("\r\n名前は" + this.name + "です");
+			System.out.println("年は" + this.age + "才です");
+			System.out.println("BMIは" +  ((double)Math.round(bmi() * 10)/10) + "です");
+			 System.out.println("合計" + Person.count + "人です");
+		}
+		
+		
+}
