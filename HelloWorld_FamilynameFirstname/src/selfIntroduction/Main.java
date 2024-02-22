ae785304e225be6d50dd1df98268bda7af3c8f03
nmsoa <nmsoa@GeimiBookPro02>
2024-02-22 16:01:57 +0900

    JAVA 1.31 Main

--- HelloWorld_FamilynameFirstname/src/selfIntroduction/Main.java
+++ HelloWorld_FamilynameFirstname/src/selfIntroduction/Main.java
@@ -3,13 +3,21 @@ package selfIntroduction;
 public class Main {
 
 	public static void main(String[] args) {
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		Person person1 = new Person("鈴木" , "太郎", 20, 1.7, 60);
 
-		System.out.println(person1.name);
-		System.out.println(person1.age);
-		System.out.println(person1.height);
-		System.out.println(person1.weight);
-		
-		person1.print();
+
+		Person person2 = new Person("山田", "花子", 22, 1.5, 40);
+
+		Car car = new Car();
+		Bicycle bicycle = new Bicycle();
+
+		car.setOwner(person1.fullName());
+		bicycle.setOwner(person2.fullName());
+
+		System.out.println(car.getOwner());   
+		System.out.println(bicycle.getOwner());
+
+		person1.buy(car);
+		person2.buy(bicycle);
 	}
 }
