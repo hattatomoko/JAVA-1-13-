410b2dbd3348218d3b7eb7addd3e8465da4ac221
nmsoa <nmsoa@GeimiBookPro02>
2024-02-22 15:16:32 +0900

    JAVA 自己紹介 Main

--- HelloWorld_FamilynameFirstname/src/selfIntroduction/Main.java
+++ HelloWorld_FamilynameFirstname/src/selfIntroduction/Main.java
@@ -3,7 +3,6 @@ package selfIntroduction;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 
 		System.out.println(person1.name);
