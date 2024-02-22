29af85fd3ec1cecdf413f08848ed9effcc7ec355
nmsoa <nmsoa@GeimiBookPro02>
2024-02-22 16:03:06 +0900

    JAVA 1.31 Vehicle

---
+++ HelloWorld_FamilynameFirstname/src/selfIntroduction/Vehicle.java
@@ -0,0 +1,15 @@
+package selfIntroduction;
+
+public class Vehicle {
+	
+	private String owner;
+	
+	public String getOwner() {
+		return owner;
+		}
+
+		public void setOwner(String owner) {
+		this.owner = owner;
+		}
+
+}
