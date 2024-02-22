273557637d2362893e7a70d528210d4fd7926583
nmsoa <nmsoa@GeimiBookPro02>
2024-02-22 16:02:36 +0900

    JAVA 1.31 Person

--- HelloWorld_FamilynameFirstname/src/selfIntroduction/Person.java
+++ HelloWorld_FamilynameFirstname/src/selfIntroduction/Person.java
@@ -2,38 +2,60 @@ package selfIntroduction;
 
 public class Person {
 	// インスタンスフィールドを定義
-	public String name;
+	public static int count = 0;
+	public String firstName;
+	public String lastName;
 	public int age;
-	public double height;
-	public double weight;
-	public double bmi;
-	public int print;
-	public static int count;
+	public double height, weight;
 
-
-
-	// コンストラクタを定義しインスタンスフィールドに値をセット
-	Person(String name, int age, double height,double weight){
-		this.name = name;
+	// コンストラクタの作成
+	Person(String firstName, int age, double height, double weight){
+		this.firstName = firstName;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
-		Person.count ++;
+		Person.count++;
 	}
 
-	// bmiの定義
-	public double bmi() {
-		return this.weight / (this.height * this.height);
-
+	// コンストラクタのオーバーロード
+	Person(String firstName,String lastName, int age, double height, double weight){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		Person.count++;
 	}
 
-	// インスタンスメソッドprintから（戻り値void)
-	public void print() {
-		System.out.println("\r\n名前は" + this.name + "です");
-		System.out.println("年は" + this.age + "才です");
-		System.out.println("BMIは" +  (double)Math.floor(bmi()) + "です");
+	// インスタンスメソッドfullNameを定義
+	public String fullName(){
+		return this.firstName + this.lastName;
+	}
+
+	// インスタンスメソッドprintを定義
+	public void print(){
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+
+	// インスタンスメソッドbmiを定義
+	public double bmi(){
+		return this.weight / this.height / this.height;
+	}
+	
+	public static void printCount(){
 		System.out.println("合計" + Person.count + "人です");
 	}
+	
+	// インスタンスメソッドbuyを定義
+	public void buy(Car car) {
+		car.setOwner(this.fullName());
+		System.out.println(car.getOwner() + "が購入しました");
+	}
 
-
+	public void buy(Bicycle bicycle) {
+		bicycle.setOwner(this.fullName());
+		System.out.println(bicycle.getOwner() + "が購入しました");
+	}
 }
+
